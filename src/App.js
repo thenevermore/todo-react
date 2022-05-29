734edb8dcfc0c1af2ed0f8c078cfb3286474def9
Monica Kristiadi <monica.kristiadi@tech.jago.com>
2022-05-30 00:58:15 +0700

    add mui, roboto font, and fix css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import React from 'react';
 
 import ToDo from './containers/ToDo';
+
 
 const App = () => {
   return (
